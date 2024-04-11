3a12c3ca705c4a7a981993054152e4fdcd225258
Gilberto Ribeiro <gillbertosr5@gmail.com>
2024-04-10 22:15:20 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div className="AppContainer">
+      <header className="header">
+        <nav>
+          <a className="logo" href="#">
+            MyPage
+          </a>
+          <ul>
+            <li className="nav-items">
+              <a className="nav-link" href="#">
+                Projects
+              </a>
+            </li>
+            <li className="nav-items">
+              <a className="nav-link" href="#">
+                Knowledge
+              </a>
+            </li>
+            <li className="nav-items">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
